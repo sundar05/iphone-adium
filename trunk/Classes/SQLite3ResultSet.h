3d04b599e72db152754a75e6dedcdacfe154e477
nganpham <nganpham@ec8afdfd-674b-0410-8119-53b98edb4c42>
2008-04-25 00:21:20 +0000

    Added SQLite3 classes.

---
+++ trunk/Classes/SQLite3ResultSet.h
@@ -0,0 +1,57 @@
+//
+//  SQLite3ResultSet.h
+//  MobileAdium
+//
+//  Created by Ngan Pham on 4/24/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "sqlite3.h"
+
+@class SQLite3Database;
+
+@interface SQLite3ResultSet : NSObject {
+  SQLite3Database *parentDB;
+  sqlite3_stmt *pStmt;
+  NSString *query;
+  NSMutableDictionary *columnNameToIndexMap;
+  BOOL columnNamesSetup;
+}
+
++ (id) resultSetWithStatement:(sqlite3_stmt *)stmt usingParentDatabase:(SQLite3Database *)aDB;
+
+- (void) close;
+
+- (NSString *)query;
+- (void)setQuery:(NSString *)value;
+
+- (void)setPStmt:(sqlite3_stmt *)newsqlite3_stmt;
+- (void)setParentDB:(SQLite3Database *)newDb;
+
+- (BOOL) next;
+
+- (int) intForColumn:(NSString *)columnName;
+- (int) intForColumnIndex:(int)columnIdx;
+
+- (long) longForColumn:(NSString *)columnName;
+- (long) longForColumnIndex:(int)columnIdx;
+
+- (BOOL) boolForColumn:(NSString *)columnName;
+- (BOOL) boolForColumnIndex:(int)columnIdx;
+
+- (double) doubleForColumn:(NSString *)columnName;
+- (double) doubleForColumnIndex:(int)columnIdx;
+
+- (NSString *) stringForColumn:(NSString *)columnName;
+- (NSString *) stringForColumnIndex:(int)columnIdx;
+
+- (NSDate *) dateForColumn:(NSString *)columnName;
+- (NSDate *) dateForColumnIndex:(int)columnIdx;
+
+- (NSData *) dataForColumn:(NSString *)columnName;
+- (NSData *) dataForColumnIndex:(int)columnIdx;
+
+- (void) kvcMagic:(id)object;
+
+@end
