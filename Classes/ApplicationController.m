02ae88a507b1d623dee02d5e9553a0a2fb2df53b
nganpham <nganpham@ec8afdfd-674b-0410-8119-53b98edb4c42>
2008-04-26 07:08:23 +0000

    Some logging and function renaming.

--- Classes/ApplicationController.m
+++ Classes/ApplicationController.m
@@ -11,6 +11,7 @@
 #import "AccountsNavigationController.h"
 #import "ProfileNavigationController.h"
 #import "SettingsNavigationController.h"
+#import "SQLite3Database.h"
 
 @implementation ApplicationController
 
@@ -25,7 +26,7 @@
 		// Initialize your application controller.
 		self.title = @"ApplicationController";
     [self initializeDatabase];
-		[self initializeCoreControllers];
+		[self initializeNavigationControllers];
 	}
 	return self;
 }
@@ -42,7 +43,34 @@
 	// Release anything that's not essential, such as cached data.
 }
 
-- (void)initializeCoreControllers {
+- (void)initializeDatabase {
+  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+  NSString *documentsDirectory = [paths objectAtIndex:0];
+  if (!documentsDirectory) {
+    NSLog(@"Documents directory not found.");
+    return;
+  } else {
+    NSLog(@"Documents directory found: %@", documentsDirectory);
+  }
+  
+  NSString *applicationDatabasePath = [documentsDirectory stringByAppendingPathComponent:@"data.db"];
+  NSLog(@"Application database path: %@", applicationDatabasePath);
+  
+  SQLite3Database *applicationDatabase = [SQLite3Database databaseWithPath:applicationDatabasePath];
+  if (![applicationDatabase open]) {
+    NSLog(@"Could not open application database.");
+    return;
+  }
+  // create a bad statement, just to test the error code.
+  int err = [applicationDatabase executeUpdate:@"blah blah blah"];
+  if (err) {
+    NSLog(@"Err %d: %@", [applicationDatabase lastErrorCode], [applicationDatabase lastErrorMessage]);
+  }
+}
+
+- (void)initializeNavigationControllers {
+  NSLog(@"Initializing navigation controllers.");
+
 	// initialize core controllers
 	accountsNavigationController = [[[AccountsNavigationController alloc] init] autorelease];
 	contactListNavigationController = [[[ContactListNavigationController alloc] init] autorelease];
@@ -52,9 +80,6 @@
 	// assign array to viewControllers
 	//self.viewControllers = [NSArray arrayWithObjects:accountsNavigationController, contactListNavigationController, profileNavigationController, settingsNavigationController, nil];
   self.viewControllers = [NSArray arrayWithObjects:accountsNavigationController, contactListNavigationController, nil];
-}
-
-- (void)initializeDatabase {
 }
 
 - (void)dealloc
--- Classes/SQLite3Database.h
+++ Classes/SQLite3Database.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "sqlite3.h"
+#import "SQLite3ResultSet.h"
 
 @interface SQLite3Database : NSObject {
 	sqlite3* db;
